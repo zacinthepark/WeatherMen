7581994e4211f2f6bc938bb424c5cc351caf2bc2
zacinthepark <zacinthepark@gmail.com>
2022-01-14 05:31:20 +0900

    Add tableview datasource methods to CurrentDestinationVC

--- WeatherMen/CurrentDestinationViewController.swift
+++ WeatherMen/CurrentDestinationViewController.swift
@@ -8,6 +8,10 @@
 import UIKit
 
 class CurrentDestinationViewController: UIViewController {
+    
+    @IBOutlet weak var listTableView: UITableView!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,4 +30,41 @@ class CurrentDestinationViewController: UIViewController {
     }
     */
 
+}
+
+extension CurrentDestinationViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 2
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        switch section {
+        case 0:
+            return 1
+        case 1:
+            return 0
+        default:
+            return 0
+        }
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        if indexPath.section == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "SummaryTableViewCell", for: indexPath) as! SummaryTableViewCell
+            
+            return cell
+        }
+    
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ForecastTableViewCell", for: indexPath) as! ForecastTableViewCell
+            
+        return cell
+    
+    }
+    
 }
