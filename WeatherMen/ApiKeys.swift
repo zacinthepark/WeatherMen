1cec72b26fe4617c837033f4505b64af9d145cd6
zacinthepark <zacinthepark@gmail.com>
2022-03-03 16:42:00 +0900

    Add location converter to grid

--- WeatherMen/ApiKeys.swift
+++ WeatherMen/ApiKeys.swift
@@ -8,3 +8,4 @@
 import Foundation
 
 let openWeatherMapApiKey = "9bb607c5051f148d38ced029dd8953fd"
+let kmaApiKey = "%2FeMAYmd0WnFuC32JC49oHVkfAU7hIksECKvNI1BTm1BPeM1cRaT8LmXPeOAoEXTqfj7S7sxDD6QRQsZpgZDzpg%3D%3D"
