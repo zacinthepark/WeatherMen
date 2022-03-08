5e4e3fb1f6645e41ee587df7b56f31a808591ce2
zacinthepark <zacinthepark@gmail.com>
2022-03-08 19:43:38 +0900

    Change accuweather apikey to standard call service

--- WeatherMen/Model/ApiKeys.swift
+++ WeatherMen/Model/ApiKeys.swift
@@ -8,6 +8,6 @@
 import Foundation
 
 let openWeatherMapApiKey = "9bb607c5051f148d38ced029dd8953fd"
-let accuWeatherApiKey = "bHksqumX7phbLMJ6RL2i2TRv384fK5Uq"
+let accuWeatherApiKey = "gduZ8vN55jfW18WA97rRsVCi2UdlO2wp"
 let kmaApiKey = "%2FeMAYmd0WnFuC32JC49oHVkfAU7hIksECKvNI1BTm1BPeM1cRaT8LmXPeOAoEXTqfj7S7sxDD6QRQsZpgZDzpg%3D%3D"
 
